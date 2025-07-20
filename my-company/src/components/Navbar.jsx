73954669237a9963d1ea34b38fe8d526d3f399c1
Jesse Ubani <ubanijesse@gmail.com>
2025-07-21 00:00:07 +0100

    revert to old style

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,23 +1,23 @@
 import React from "react";
 
 function Navbar() {
-    return (
-        <nav
-            style={{
-                display: "flex",
-                justifyContent: "space-between",
-                backgroundColor: "#eee",
-                padding: "10px 20px"
-            }}
-        >
-            <div>Logo</div>
-            <div>
-                <a href="/">Home</a>{" | "}
-                <a href="/about">About</a>{" | "}
-                <a href="/contact">Contact</a>
-            </div>
-        </nav>
-    );
+  return (
+    <nav
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        backgroundColor: "#eee",
+        padding: "10px 20px"
+      }}
+    >
+      <div>Logo</div>
+      <div>
+        <a href="/">Home</a>{" | "}
+        <a href="/about">About</a>{" | "}
+        <a href="/contact">Contact</a>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
