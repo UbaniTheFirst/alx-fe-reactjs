d22d4a591e7fde970794da67856d20e2bc0a90ab
Jesse Ubani <ubanijesse@gmail.com>
2025-07-27 00:05:17 +0100

    edit

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,6 +2,7 @@ import { create } from 'zustand';
 
 const useRecipeStore = create((set) => ({
     recipes: [],
+    setRecipes: (recipes) => set({ recipes }),
     addRecipe: (recipe) =>
         set((state) => ({
             recipes: [...state.recipes, recipe],
